97b233d7b422f8af1b060d2afa8f397298c3187a
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-08-25 00:52:45 +0700

    memoize selectExistingCategories using reselect

--- src/pages/categories/index.jsx
+++ src/pages/categories/index.jsx
@@ -29,14 +29,9 @@ const CategoriesPage = () => {
       <section className='flex flex-col gap-4 items-center'>
         <h2 className='text-lg md:text-2xl font-semibold'>{categoryNavTitle}</h2>
         <ul className='flex gap-4 flex-wrap'>
-          {existingCategories.map((existingCategory) => (
-            <li><Link to={`/categories/${existingCategory}`} className='border-2 px-2 py-1 hover:border-black transition delay-75'>{existingCategory.charAt(0).toUpperCase() + existingCategory.slice(1)}</Link></li>
+          {existingCategories.map((existingCategory, index) => (
+            <li key={index}><Link to={`/categories/${existingCategory}`} className='border-2 px-2 py-1 hover:border-black transition delay-75'>{existingCategory.charAt(0).toUpperCase() + existingCategory.slice(1)}</Link></li>
           ))}
-          {/* <li><Link to="/categories/furnitures" className='border-2 px-2 py-1 hover:border-black transition delay-75'>Furnitures</Link></li>
-          <li><Link to="/categories/electronics" className='border-2 px-2 py-1 hover:border-black transition delay-75'>Electronics</Link></li>
-          <li><Link to="/categories/lamps" className='border-2 px-2 py-1 hover:border-black transition delay-75'>Lamps</Link></li>
-          <li><Link to="/categories/apparel" className='border-2 px-2 py-1 hover:border-black transition delay-75'>Apparel</Link></li>
-          <li><Link to="/categories/beauty" className='border-2 px-2 py-1 hover:border-black transition delay-75'>Beauty</Link></li> */}
         </ul>
       </section>
       {productSection}
