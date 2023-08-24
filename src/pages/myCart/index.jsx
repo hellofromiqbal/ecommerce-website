bfc1b8e31442ee7c66fa8120cf6e470f1ac57b51
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-08-24 11:29:34 +0700

    handle empty miCart UI

--- src/pages/myCart/index.jsx
+++ src/pages/myCart/index.jsx
@@ -6,20 +6,24 @@ import { selectAllProducts } from '../../redux/products/productsSlice';
 
 const MyCartPage = () => {
   const allProducts = useSelector(selectAllProducts);
+  const productsOnCart = allProducts.filter((product) => product.amountOnCart > 0);
   return (
     <section className='min-h-screen'>
-      <ProductList>
-        {allProducts.map((product) => {
-          if (product.amountOnCart !== 0) {
-            return (
-              <ProductCard
-                key={product.id}
-                product={product}
-              />
-            );
-          }
-        })}
-      </ProductList>
+      {productsOnCart.length < 1 ?
+        <div className='flex flex-col md:w-2/3 lg:w-full justify-center gap-4 text-start m-auto'>
+          <h1 className='text-2xl md:text-5xl font-semibold'>No products on cart yet!</h1>
+          <p className='text-sm md:text-base text-black text-opacity-80'>You can add to cart a product by clicking "ADD TO CART" button on the product detail page.</p>
+        </div>
+        :
+        <ProductList>
+          {productsOnCart.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+            />
+          ))}
+        </ProductList>
+      }
     </section>
   );
 };
