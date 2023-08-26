2ed9d0d78a8eff38551b5b35bda5afa962d997f2
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-08-26 17:06:38 +0700

    fix add to cart & amount counter functionalites

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -40,7 +40,8 @@ const DetailPage = () => {
 
   const product = allProducts.find((product) => product.id === id);
 
-  const [counter, setCounter] = useState(product.amountOnCart < 1 ? 1 : product.amountOnCart);
+  // const [counter, setCounter] = useState(product.amountOnCart < 1 ? 1 : product.amountOnCart);
+  const [counter, setCounter] = useState(1);
 
   const handleAddToCart = () => {
     if (product.stock < 1) {
@@ -66,14 +67,14 @@ const DetailPage = () => {
 
   const decrementCounter = () => {
     setCounter((prev) => prev - 1);
-    dispatch(decrementAmountOnCart(product.id, counter));
+    // dispatch(decrementAmountOnCart(product.id, counter));
   };
 
   const incrementCounter = () => {
     setCounter((prev) => prev + 1);
-    if (product.amountOnCart > 0) {
-      dispatch(incrementAmountOnCart(product.id, counter));
-    }
+    // if (product.amountOnCart > 0) {
+    //   dispatch(incrementAmountOnCart(product.id, counter));
+    // }
   };
 
   return (
@@ -100,8 +101,9 @@ const DetailPage = () => {
         <div className='basis-1/2 bg-gray-200 flex flex-col justify-center p-4 md:p-10 gap-10'>
           <p className='font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio sequi dolores iusto expedita, pariatur quibusdam molestiae inventore ad voluptatem veritatis?</p>
           <p className='text-lg font-semibold'>Stock: {product.stock}</p>
+          <p className='text-lg font-semibold'>Amount on cart: {product.amountOnCart}</p>
           <div className='flex flex-col md:flex-row gap-4 justify-between items-center'>
-            <h2 className='text-lg md:text-2xl font-semibold text-center'>Quantity</h2>
+            <h2 className='text-lg md:text-2xl font-semibold text-center'>Add qty to cart</h2>
             <AmountCounter
               product={product}
               counter={counter}
@@ -115,7 +117,7 @@ const DetailPage = () => {
               dimensionStyle="p-3 w-full"
               text="ADD TO CART"
               onClick={handleAddToCart}
-              disabled={product.stock < 1}
+              disabled={product.stock < 1 || product.amountOnCart === product.stock}
             />
             <Button
               borderStyle="border-2 border-transparent hover:border-red-700"
