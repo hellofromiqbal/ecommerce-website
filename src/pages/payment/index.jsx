8c640385f668403307d893bb8e444e60bff2f2f5
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-08-26 17:29:45 +0700

    edit payment form

--- src/pages/payment/index.jsx
+++ src/pages/payment/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../components/elements/button';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -15,6 +15,17 @@ const PaymentPage = () => {
   const allProducts = useSelector(selectAllProducts);
 
   const product = allProducts.find((product) => product.id === id);
+
+  const [deliveryMethod, setDeliveryMethod] = useState('');
+
+  const deliveryFee = () => {
+    if (deliveryMethod === 'Regular') return 10;
+    if (deliveryMethod === 'Cargo') return 15;
+    if (deliveryMethod === 'Next Day') return 20;
+    return 0;
+  };
+
+  const [paymentMethod,  setPaymentMethod] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -59,35 +70,35 @@ const PaymentPage = () => {
             <div className='flex flex-col gap-4'>
               <div className='flex flex-col'>
                 <label htmlFor="name" className="text-sm md:text-base">Name:</label>
-                <input id='name' type="text" className='border-2 border-gray-200 focus:border-black px-2 py-1'/>
+                <input id='name' type="text" className='border-2 border-gray-200 focus:border-black px-2 py-1' required/>
               </div>
               <div className='flex flex-col'>
                 <label htmlFor="email" className="text-sm md:text-base">Email:</label>
-                <input id='email' type="email" className='border-2 border-gray-200 focus:border-black px-2 py-1'/>
+                <input id='email' type="email" className='border-2 border-gray-200 focus:border-black px-2 py-1' required/>
               </div>
               <div className='flex flex-col'>
                 <label htmlFor="phone" className="text-sm md:text-base">Phone:</label>
-                <input id='phone' type="number" className='border-2 border-gray-200 focus:border-black px-2 py-1'/>
+                <input id='phone' type="text" className='border-2 border-gray-200 focus:border-black px-2 py-1' required/>
               </div>
               <div className='flex flex-col'>
                 <label htmlFor="address" className="text-sm md:text-base">Address:</label>
-                <textarea name="address" id="address" className='border-2 border-gray-200 focus:border-black px-2 py-1 resize-none'></textarea>
+                <textarea name="address" id="address" className='border-2 border-gray-200 focus:border-black px-2 py-1 resize-none' required></textarea>
               </div>
               <div className='flex flex-col'>
                 <label htmlFor="deliveryMethod" className="text-sm md:text-base">Delivery Method</label>
-                <select name="deliveryMethod" id="deliveryMethod" className='border-2 border-gray-200 focus:border-black px-2 py-1'>
+                <select name="deliveryMethod" id="deliveryMethod" className='border-2 border-gray-200 focus:border-black px-2 py-1' onChange={(e) => setDeliveryMethod(e.target.value)} required>
                   <option value="" className='text-sm md:text-base'>-- Select delivery method --</option>
-                  <option value="regular" className='text-sm md:text-base'>Regular</option>
-                  <option value="cargo" className='text-sm md:text-base'>Cargo</option>
-                  <option value="nextDay" className='text-sm md:text-base'>Next Day</option>
+                  <option value="Regular" className='text-sm md:text-base'>Regular</option>
+                  <option value="Cargo" className='text-sm md:text-base'>Cargo</option>
+                  <option value="Next Day" className='text-sm md:text-base'>Next Day</option>
                 </select>
               </div>
               <div className='flex flex-col'>
                 <label htmlFor="paymentMethod" className="text-sm md:text-base">Payment Method</label>
-                <select name="paymentMethod" id="paymentMethod" className='border-2 border-gray-200 focus:border-black px-2 py-1'>
+                <select name="paymentMethod" id="paymentMethod" className='border-2 border-gray-200 focus:border-black px-2 py-1' onChange={(e) => setPaymentMethod(e.target.value)} required>
                   <option value="" className='text-sm md:text-base'>-- Select payment method --</option>
-                  <option value="bankTransfer" className='text-sm md:text-base'>Bank Transfer</option>
-                  <option value="cashOnDelivery" className='text-sm md:text-base'>Cash On Delivery</option>
+                  <option value="Bank Transfer" className='text-sm md:text-base'>Bank Transfer</option>
+                  <option value="Cash On Delivery" className='text-sm md:text-base'>Cash On Delivery</option>
                 </select>
               </div>
             </div>
@@ -129,24 +140,19 @@ const PaymentPage = () => {
               </div>
             </div>
             <div className='flex flex-col'>
-              <p className='font-medium text-md md:text-lg'>Delivery Fee</p>
-              <p className='text-sm md:text-base text-black text-opacity-80'>Delivery Method</p>
+              <p className='font-medium text-md md:text-lg'>Delivery Method</p>
               <div className='flex justify-between'>
-                <p className='text-sm md:text-base text-black text-opacity-80'>Regular</p>
-                <p className='text-sm md:text-base font-medium'>$10</p>
+                <p className='text-sm md:text-base text-black text-opacity-80'>{deliveryMethod}</p>
+                <p className='text-sm md:text-base font-medium'>${deliveryFee()}</p>
               </div>
             </div>
             <div className='flex flex-col'>
-              <p className='font-medium text-md md:text-lg'>Payment Fee</p>
-              <p className='text-sm md:text-base text-black text-opacity-80'>Payment Method</p>
-              <div className='flex justify-between'>
-                <p className='text-sm md:text-base text-black text-opacity-80'>Cash On Delivery</p>
-                <p className='text-sm md:text-base font-medium'>$0</p>
-              </div>
+              <p className='font-medium text-md md:text-lg'>Payment Method</p>
+              <p className='text-sm md:text-base text-black text-opacity-80'>{paymentMethod}</p>
             </div>
             <div className='flex justify-between items-center'>
               <p className='font-medium text-md md:text-lg'>Total Payment</p>
-              <p className='font-bold text-xl'>${(product.amountOnCart * product.price) + 10}</p>
+              <p className='font-bold text-xl'>${(product.amountOnCart * product.price) + deliveryFee()}</p>
             </div>
           </div>
           <div className='flex justify-end lg:justify-normal'>
