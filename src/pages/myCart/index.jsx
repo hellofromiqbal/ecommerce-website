63424c5faea4509eed9c3119efc26d9877e9ab2b
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-08-28 12:05:31 +0700

    adjust My Cart page

--- src/pages/myCart/index.jsx
+++ src/pages/myCart/index.jsx
@@ -7,6 +7,10 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const MyCartPage = () => {
+  const allProducts = useSelector(selectAllProducts);
+
+  const productsOnCart = allProducts.filter((product) => product.amountOnCart > 0);
+
   const notify = (message) => toast.success(message, {
     position: "top-center",
     autoClose: 500,
@@ -18,30 +22,21 @@ const MyCartPage = () => {
     theme: "dark",
     });
 
-  const allProducts = useSelector(selectAllProducts);
-
-  const productsOnCart = allProducts.filter((product) => product.amountOnCart > 0);
-
   return (
-    <section className='min-h-screen'>
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      {productsOnCart.length < 1 ?
-        <div className='flex flex-col md:w-2/3 lg:w-full justify-center gap-4 text-start lg:m-auto'>
-          <h1 className='text-2xl md:text-5xl font-semibold'>No products on cart yet!</h1>
-          <p className='text-sm md:text-base text-black text-opacity-80'>You can add to cart a product by clicking "ADD TO CART" button on the product detail page.</p>
-        </div>
-        :
+    <>
+      <section className='min-h-screen'>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
         <ProductList>
           {productsOnCart.map((product) => (
             <ProductCard
@@ -51,8 +46,8 @@ const MyCartPage = () => {
             />
           ))}
         </ProductList>
-      }
-    </section>
+      </section>
+    </>
   );
 };
 
