3dc227915f0b6fe14936ba0642ae8cb231dd6d26
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-08-28 13:43:37 +0700

    delete unused redux slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsSlice from "./products/productsSlice";
-import cartSlice from "./cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
     products: productsSlice,
-    cart: cartSlice
   }
 });
