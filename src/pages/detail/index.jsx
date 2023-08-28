9744c4aa2e563b8392452ef1a69aa6182245ba05
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-08-28 12:00:11 +0700

    add product doesn't exist

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,146 +1,27 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
-import { addToCart, selectAllProducts } from '../../redux/products/productsSlice';
-import AmountCounter from '../../components/fragments/amountCounter';
-import Button from '../../components/elements/button';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { selectAllProducts } from '../../redux/products/productsSlice';
+import ProductDetailSection from './productDetailSection';
 
 const DetailPage = () => {
   const { id } = useParams();
-
-  const navigate = useNavigate();
-
-  const dispatch = useDispatch();
-
-  const notifySuccess = (message) => toast.success(message, {
-    position: "top-center",
-    autoClose: 1000,
-    hideProgressBar: true,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "dark",
-  });
-
-  const notifyFailed = (message) => toast.error(message, {
-    position: "top-center",
-    autoClose: 1000,
-    hideProgressBar: true,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "dark",
-  });
 
   const allProducts = useSelector(selectAllProducts);
 
   const product = allProducts.find((product) => product.id === id);
 
-  const [counter, setCounter] = useState(1);
-
-  const handleAddToCart = () => {
-    if (product.stock < 1) {
-      notifyFailed(`${product.name} has 0 stock. Cannot add to cart!`);
-    } else {
-      dispatch(addToCart(product.id, counter));
-      notifySuccess(`${counter} ${product.name}(s) has been added to cart!`);
-    }
-  };
-
-  const handleMakeOrder = () => {
-    if (product.stock < 1) {
-      notifyFailed(`${product.name} has 0 stock. Cannot make order!`);
-    } else {
-      if (product.amountOnCart < 1) {
-        dispatch(addToCart(product.id, counter));
-        navigate(`/payment/${product.id}`);
-      } else {
-        navigate(`/payment/${product.id}`);
-      }
-    }
-  };
-
-  const decrementCounter = () => {
-    setCounter((prev) => prev - 1);
-  };
-
-  const incrementCounter = () => {
-    setCounter((prev) => prev + 1);
-  };
-
   return (
-    <section className='flex flex-col gap-4 md:relative'>
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      <h1 className='text-xl md:text-3xl font-bold text-center md:absolute left-0 right-0 m-auto'>{product.name}</h1>
-      <div className='md:h-[35rem] flex flex-col md:flex-row gap-4'>
-        <div className='basis-1/2 flex flex-col justify-center items-center'>
-          <div className='basis-1/2 flex'>
-            <img src={product.imgUrl} alt={product.name} className='md:max-w-[300px]'/>
-          </div>
-        </div>
-        <div className='basis-1/2 bg-gray-200 flex flex-col justify-center p-4 md:p-10 gap-10'>
-          <p className='font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio sequi dolores iusto expedita, pariatur quibusdam molestiae inventore ad voluptatem veritatis?</p>
-          <p className='text-lg font-semibold'>Stock: {product.stock}</p>
-          <p className='text-lg font-semibold'>Amount on cart: {product.amountOnCart}</p>
-          <div className='flex flex-col md:flex-row gap-4 justify-between items-center'>
-            <h2 className='text-lg md:text-2xl font-semibold text-center'>Add qty to cart</h2>
-            <AmountCounter
-              product={product}
-              counter={counter}
-              decrementCounter={decrementCounter}
-              incrementCounter={incrementCounter}
-            />
-            <h2 className='text-lg md:text-2xl font-semibold text-center'>${product.price * counter}</h2>
-          </div>
-          <div className='grid grid-cols-2 md:flex-row gap-4'>
-            <Button
-              dimensionStyle="p-3 w-full"
-              text="ADD TO CART"
-              onClick={handleAddToCart}
-              disabled={product.stock < 1 || product.amountOnCart === product.stock}
-            />
-            <Button
-              borderStyle="border-2 border-transparent hover:border-red-700"
-              bgColorStyle="bg-red-700 hover:bg-transparent"
-              colorStyle="text-white hover:text-red-700"
-                dimensionStyle="p-3 w-full"
-              text="MAKE ORDER"
-              onClick={handleMakeOrder}
-              disabled={product.stock < 1}
-            />
-          </div>
-        </div>
+    <>
+    {product ?
+      <ProductDetailSection product={product}/>
+      :
+      <div className='flex flex-col md:w-2/3 lg:w-full min-h-screen gap-4 text-start lg:m-auto'>
+        <h1 className='text-2xl md:text-5xl font-semibold'>Product doesn't exist!</h1>
+        <p className='text-sm md:text-base text-black text-opacity-80'>We don't have the product you were looking for. Please check again!</p>
       </div>
-      <div className='grid md:grid-cols-3 gap-4'>
-        <div className='flex flex-col gap-2 bg-gray-200 p-4'>
-          <h2 className='text-lg md:text-xl font-semibold'>Texture:</h2>
-          <p>{product.texture}</p>
-        </div>
-        <div className='flex flex-col gap-2 bg-gray-200 p-4'>
-          <h2 className='text-lg md:text-xl font-semibold'>Weight:</h2>
-          <p>{product.weight}</p>
-        </div>
-        <div className='flex flex-col gap-2 bg-gray-200 p-4'>
-          <h2 className='text-lg md:text-xl font-semibold'>Size:</h2>
-          <p>{product.size}</p>
-        </div>
-      </div>
-    </section>
+    }
+    </>
   )
 };
 
--- src/pages/myCart/index.jsx
+++ src/pages/myCart/index.jsx
@@ -37,7 +37,7 @@ const MyCartPage = () => {
         theme="dark"
       />
       {productsOnCart.length < 1 ?
-        <div className='flex flex-col md:w-2/3 lg:w-full justify-center gap-4 text-start m-auto'>
+        <div className='flex flex-col md:w-2/3 lg:w-full justify-center gap-4 text-start lg:m-auto'>
           <h1 className='text-2xl md:text-5xl font-semibold'>No products on cart yet!</h1>
           <p className='text-sm md:text-base text-black text-opacity-80'>You can add to cart a product by clicking "ADD TO CART" button on the product detail page.</p>
         </div>
